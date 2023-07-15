c9e176a4affbacd7ef6f29067f4729794721c130
Gacs Péter <g.silver.014@gmail.com>
2023-07-15 16:12:29 +0200

    Main function works

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from Api.Weather import Weather
+from App.Separator import Separator
 
 def main():
     city = input('Kérem a város nevét: ')
 
     precipitations = Weather.new().of(city).getPrecipitations()
 
+    Separator(precipitations).showResult()
+
     return precipitations
 
-print(main())
+main()
